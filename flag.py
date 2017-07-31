5a2ab53c3fd2e8bc79f3082e76267e1f15b49381
MICHELLE GALYARDT <30426608+michellegal@users.noreply.github.com>
2017-07-30 23:40:27 -0700

    Update flag.py

---
+++ flag.py
@@ -0,0 +1,9 @@
+for s in range (4,0,-1):
+    for e in range (1,0,-1):
+        print((7-e) * '*  ' + (40-e) * '=')
+        print((6-e) * ' * ' + (3 * ' ') + (40-e) * '=')
+for e in range (1,0,-1):
+        print((7-e) * '*  ' + (40-e) * '=')
+
+for p in range (4):
+        print(57 * '=')
